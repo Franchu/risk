66a1064fcf9030e455e9f4724fbc030234e21915
Francisco Paradela <fparafer@gmail.com>
2017-09-01 14:15:25 +0200

    Territorios empiezan con 10 ejércitos

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ var dice = new Dice(6);
 var world = new World();
 
 $(document).ready(function() {
-  world.createTerritory('mordor', 6);
-  world.createTerritory('rohan', 3);
-  world.createTerritory('shire', 2);
-  world.createTerritory('gondor', 5);
-  world.createTerritory('rivendel', 3);
+  world.createTerritory('mordor', 10);
+  world.createTerritory('rohan', 10);
+  world.createTerritory('shire', 10);
+  world.createTerritory('gondor', 10);
+  world.createTerritory('rivendel', 10);
 
   $('.territory').on('click', function() {
     world.getTerritoryName($(this));
